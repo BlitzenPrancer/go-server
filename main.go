14313138a2b199bd82dd8c7a3e7a0edfb1843f8f
Blitzen Prancer <blitzenprancer666@gmail.com>
2021-08-13 12:07:31 +0530

    added helloHandler function

--- main.go
+++ main.go
@@ -6,6 +6,18 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "this method you have entered is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello!")
+}
+
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	// start handling my route route and sending that to fileServer
